b781d38c093e0dbd61fbb50da963ec7c12d49003
Bloodeyes00 <zebs2211@gmail.com>
2023-01-21 02:21:05 +0500

    Successfully Pushed

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 #!/bin/bash 
 # Deployment script for GitHub 
 # Push all changes to GitHub 
-git remote add origin https://github.com/Bloodeyes00/Ezi-Tech.git
+git remote add origin https://github.com/Bloodeyes00/Ecommerse-Web.git
 git add .
 git commit -m "Successfully Pushed"
-git push -u origin master 
+git push -u origin shahrukh
 
